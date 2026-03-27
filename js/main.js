ad9663b423785e943bef2fb591cff09bbdf37196
Drew Dart <amdart1002@outlook.com>
2026-03-26 19:57:40 -0500

    Activity 9
    Activity 9

--- js/main.js
+++ js/main.js
@@ -1,131 +1,53 @@
-window.onload = function(){
-    var w = 900, h = 500;
-    
-    // container block
-    var container = d3.select("body")
+// begin script when window loads
+window.onload = setMap();
+
+function setMap() {
+
+    // map frame dimensions
+    var width = 960,
+        height = 460;
+
+    // create new svg container for map
+    var map = d3.select("body")
         .append("svg")
-        .attr("width", w)
-        .attr("height", h)
-        .attr("class", "container")
-        .style("background-color", "rgba(0,0,0,0.2)");
-    
-    // innerRect block
-    var innerRect = container.append("rect")
-        .datum(400)
-        .attr("width", function(d){
-            return d * 2;
-        })
-        .attr("height", function(d){
-            return d;
-        })
-        .attr("class", "innerRect")
-        .attr("x", 50)
-        .attr("y", 50)
-        .style("fill", "#FFFFFF");
-    
-    // city data
-    var cityPop = [
-        {city: 'Madison', population: 233209},
-        {city: 'Milwaukee', population: 594833},
-        {city: 'Green Bay', population: 104057},
-        {city: 'Superior', population: 27244}
+        .attr("class", "map")
+        .attr("width", width)
+        .attr("height", height);
+
+    // create Equal Earth projection centered on the world
+    var projection = d3.geoEqualEarth()
+        .scale(160)
+        .translate([width / 2, height / 2]);
+
+    // create path generator
+    var path = d3.geoPath()
+        .projection(projection);
+
+// set up choropleth map
+    var promises = [
+        d3.csv("data/MedalData.csv"),
+        d3.json("data/WorldCountries.topojson")
     ];
+    Promise.all(promises).then(callback);
 
-    // min and max population
-    var minPop = d3.min(cityPop, function(d){
-        return d.population;
-    });
-    var maxPop = d3.max(cityPop, function(d){
-        return d.population;
-    });
+    function callback(data) {
+        csvData = data[0];
+        countries = data[1];
 
-    // x scale
-    var x = d3.scaleLinear()
-        .range([90, 750])
-        .domain([0, 3]);
+        // translate countries topojson to geojson
+        var worldCountries = topojson.feature(countries, countries.objects.ne_110m_admin_0_countries);
 
-    // y scale
-    var y = d3.scaleLinear()
-        .range([450, 50])
-        .domain([0, 700000]);
+        // add countries to map
+        var countryPaths = map.selectAll(".countries")
+            .data(worldCountries.features)
+            .enter()
+            .append("path")
+            .attr("class", function(d) {
+                return "countries " + d.properties.NAME;
+            })
+            .attr("d", path);
 
-    // color scale
-    var color = d3.scaleLinear()
-        .range(["#FDBE85", "#D94701"])
-        .domain([minPop, maxPop]);
-
-    // number format
-    var format = d3.format(",");
-
-    // circles block
-    var circles = container.selectAll(".circles")
-        .data(cityPop)
-        .enter()
-        .append("circle")
-        .attr("class", "circles")
-        .attr("id", function(d){
-            return d.city;
-        })
-        .attr("r", function(d){
-            var area = d.population * 0.01;
-            return Math.sqrt(area / Math.PI);
-        })
-        .attr("cx", function(d, i){
-            return x(i);
-        })
-        .attr("cy", function(d){
-            return y(d.population);
-        })
-        .style("fill", function(d){
-            return color(d.population);
-        })
-        .style("stroke", "#000");
-
-    // y axis
-    var yAxis = d3.axisLeft(y);
-
-    // axis block
-    var axis = container.append("g")
-        .attr("class", "axis")
-        .attr("transform", "translate(50, 0)")
-        .call(yAxis);
-
-    // title block
-    var title = container.append("text")
-        .attr("class", "title")
-        .attr("text-anchor", "middle")
-        .attr("x", 450)
-        .attr("y", 30)
-        .text("City Populations");
-
-    // labels block
-    var labels = container.selectAll(".labels")
-        .data(cityPop)
-        .enter()
-        .append("text")
-        .attr("class", "labels")
-        .attr("y", function(d){
-            return y(d.population) - 5;
-        });
-
-    // first label
-    var nameLine = labels.append("tspan")
-        .attr("class", "nameLine")
-        .attr("x", function(d, i){
-            return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
-        })
-        .text(function(d){
-            return d.city;
-        });
-
-    //second label
-    var popLine = labels.append("tspan")
-        .attr("class", "popLine")
-        .attr("x", function(d, i){
-            return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
-        })
-        .attr("dy", "15")
-        .text(function(d){
-            return "Pop. " + format(d.population);
-        });
+        console.log(csvData);
+        console.log(countries);
+    };
 };
